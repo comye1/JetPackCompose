aaf917ca531fe7233e0c82bfbc31f57fd93a820d
comye1 <yewon2664@gmail.com>
2021-06-05 19:00:10 +0900

    Add a picture

--- JetPackComposeFirst/app/src/main/java/com/example/jetpackcomposefirst/MainActivity.kt
+++ JetPackComposeFirst/app/src/main/java/com/example/jetpackcomposefirst/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.jetpackcomposefirst
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposefirst.ui.theme.JetPackComposeFirstTheme
@@ -34,6 +36,15 @@ fun NewsStory() {
     Column (
         modifier = Modifier.padding(16.dp)
     ){
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.height(16.dp))
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
