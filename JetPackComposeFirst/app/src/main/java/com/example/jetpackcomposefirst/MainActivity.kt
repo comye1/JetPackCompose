787a3434b6e8ac141eb6fd8806ccb5383adbddaf
comye1 <yewon2664@gmail.com>
2021-06-05 19:12:13 +0900

    Apply a shape

--- JetPackComposeFirst/app/src/main/java/com/example/jetpackcomposefirst/MainActivity.kt
+++ JetPackComposeFirst/app/src/main/java/com/example/jetpackcomposefirst/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,8 +43,9 @@ fun NewsStory() {
             contentDescription = null,
             modifier = Modifier
                 .height(180.dp)
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop
+                .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(4.dp)),
+        contentScale = ContentScale.Crop
         )
         Spacer(Modifier.height(16.dp))
         Text("A day in Shark Fin Cove")
