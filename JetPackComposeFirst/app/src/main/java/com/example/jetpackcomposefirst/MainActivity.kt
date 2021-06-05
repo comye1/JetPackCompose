c08ea50462b078a8980dd33b4bd1ac38d07554d2
comye1 <yewon2664@gmail.com>
2021-06-05 18:18:25 +0900

    Add style settings to the column

--- JetPackComposeFirst/app/src/main/java/com/example/jetpackcomposefirst/MainActivity.kt
+++ JetPackComposeFirst/app/src/main/java/com/example/jetpackcomposefirst/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposefirst.ui.theme.JetPackComposeFirstTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +31,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column {
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
