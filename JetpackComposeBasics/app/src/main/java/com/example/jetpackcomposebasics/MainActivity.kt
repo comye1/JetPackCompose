20ca13cffb6a66cbb4885046e128c5b26f2982a7
comye1 <yewon2664@gmail.com>
2021-06-08 18:27:00 +0900

    Declarative UI

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -3,36 +3,64 @@ package com.example.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp{
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit){
+    JetpackComposeBasicsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+//    Column {
+//        Greeting(name = "Android")
+//        Divider(color = Color.Black)
+//        Greeting(name = "there")
+//    }
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
 }
 
 @Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasicsTheme {
-        Greeting("Android")
+    MyApp{
+        MyScreenContent()
     }
 }
