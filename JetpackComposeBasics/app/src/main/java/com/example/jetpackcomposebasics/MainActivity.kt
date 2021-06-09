0a61845e8b941c83ad4a006945ebf4474085ef20
comye1 <yewon2664@gmail.com>
2021-06-09 23:16:19 +0900

    Flexible layouts

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,22 +48,40 @@ fun Greeting(name: String) {
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit)
 {
-    Button(onClick = { updateCount(count+1)}) {
+    Button(
+        onClick = { updateCount(count+1)},
+        colors = ButtonDefaults.buttonColors(backgroundColor =
+        if(count > 5) Color.Green else Color.White)
+    ){
         Text(text = "I've been clicked $count times.")
+    }
+
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+        Greeting(name = name)
+        Divider(color = Color.Black)
+        }
     }
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
 
     val counterState = remember { mutableStateOf(0) }
 
-    Column {
-        for (name in names) {
-            Greeting(name = name)
-            Divider(color = Color.Black)
-        }
-        Divider(color = Color.Transparent, thickness = 32.dp)
+    Column (modifier = Modifier.fillMaxHeight()){
+//        Column(modifier = Modifier.weight(1f)) {
+//            for (name in names) {
+//                Greeting(name = name)
+//                Divider(color = Color.Black)
+//            }
+//        }
+        NameList(names, Modifier.weight(1f))
+//        Divider(color = Color.Transparent, thickness = 32.dp)
         Counter(
             count = counterState.value,
             updateCount = { newCount ->
