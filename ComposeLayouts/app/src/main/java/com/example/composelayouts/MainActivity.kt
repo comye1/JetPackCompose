dd31b0b6dc0d5fb5209b19fcdc50709f7434fb25
comye1 <yewon2664@gmail.com>
2021-06-13 23:19:24 +0900

    Working with lists - Display images

--- ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -20,6 +17,7 @@ import androidx.compose.material.icons.outlined.Cake
 import androidx.compose.material.icons.outlined.Face
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,15 +25,73 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
+import com.google.accompanist.coil.rememberCoilPainter
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutsTheme {
-                LazyList()
+                ImageList()
             }
         }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberCoilPainter(
+            request = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList(){
+    val listSize = 100
+
+    val scrollState = rememberLazyListState()
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column{
+        Row{
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }){
+                Text("Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }){
+                Text("Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(100){
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Preview(name = "ImageListPreview")
+@Composable
+fun ImageListPreview(){
+    ComposeLayoutsTheme {
+        ImageList()
     }
 }
 
@@ -50,7 +106,7 @@ fun LazyList(){
     }
 }
 
-@Preview(name = "LazyListPreview")
+//@Preview(name = "LazyListPreview")
 @Composable
 fun LazyListPreview(){
     ComposeLayoutsTheme {
@@ -69,7 +125,7 @@ fun SimpleList(){
     }
 }
 
-@Preview(name = "ListPreview")
+//@Preview(name = "ListPreview")
 @Composable
 fun ListPreview(){
     ComposeLayoutsTheme {
@@ -108,7 +164,7 @@ fun PhotographerCard(modifier: Modifier = Modifier){
 
 }
 
-@Preview
+//@Preview
 @Composable
 fun PhotographerCardPreview() {
     ComposeLayoutsTheme{
