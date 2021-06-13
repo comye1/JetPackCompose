fcef40ff947bf8fb3dcd9681c032be774c08f537
comye1 <yewon2664@gmail.com>
2021-06-13 22:22:25 +0900

    Working with lists - using LazyColumn

--- ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -31,12 +33,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutsTheme {
-                SimpleList()
+                LazyList()
             }
         }
     }
 }
 
+@Composable
+fun LazyList(){
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Preview(name = "LazyListPreview")
+@Composable
+fun LazyListPreview(){
+    ComposeLayoutsTheme {
+        LazyList()
+    }
+}
 @Composable
 fun SimpleList(){
 
