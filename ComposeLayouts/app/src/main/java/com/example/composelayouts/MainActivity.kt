bd1fa1a61fadefc600186cdc5a3f5cb90d5a2e4d
comye1 <yewon2664@gmail.com>
2021-06-16 22:57:18 +0900

    Intrinsics

--- ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -42,312 +42,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutsTheme {
-                LayoutsCodelab()
-            }
-        }
-    }
-}
-
-@Composable
-fun MyOwnColumn(
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier,
-        content = content
-    ) { measurables, constraints ->
-        // measure and position children given constraints logic here
-        // List of measured children
-        val placeables = measurables.map{ measurable ->
-            measurable.measure(constraints)
-        }
-
-        //Track the y co-ord we have placed children up to
-        var yPosition = 0
-
-        // Set the size of the layout as big as it can
-        layout(constraints.maxWidth, constraints.maxHeight){
-            placeables.forEach { placeable ->
-                // Position item on the screen
-                placeable.placeRelative(x = 0, y = yPosition)
-
-                // Record the y co-ord placed up to
-                yPosition += placeable.height
-            }
-        }
-    }
-}
-
-
-fun Modifier.firstBaselineToTop(
-    firstBaselineToTop : Dp
-) = this.then(
-    layout { measurable, constraints ->
-        val placeable = measurable.measure(constraints)
-        
-            //check the composable has a first baseline
-        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
-        val firstBaseline = placeable[FirstBaseline]
-        
-        // Height of the composabe with padding - first baseline
-        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
-        val height = placeable.height + placeableY
-        layout(placeable.width, height) {
-            // where the composable gets placed
-            placeable.placeRelative(0, placeableY)
-        }
-    }
-)
-@Preview("firstbaseline")
-@Composable
-fun TextWithPaddingToBaselinePreview() {
-    ComposeLayoutsTheme() {
-        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
-    }
-}
-
-@Preview("text")
-@Composable
-fun TextWithNormalPaddingPreview() {
-    ComposeLayoutsTheme() {
-        Text("Hi there!", Modifier.padding(top = 32.dp))
-    }
-}
-@Composable
-fun LayoutsCodelab(){
-    Scaffold(
-        topBar = {
-            TopAppBar (
-                title = {
-                    Text(text = "LayoutsCodelab")
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Outlined.Favorite, contentDescription = null)
-                    }
+                Surface {
+                    TwoTexts(text1 = "Hi", text2 = "there")
                 }
-            )
-        }
-    ) { innerPadding ->
-        BodyContent(
-            Modifier
-                .padding(innerPadding)
-                .padding(8.dp))
-    }
-}
-@Composable
-fun StaggeredGrid(
-    modifier: Modifier = Modifier,
-    rows: Int = 3,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier,
-        content = content
-    ) { measurables, constraints ->
-        // measure and position children given constraints logic here
-        // Keep track of the width of each row
-        val rowWidths = IntArray(rows) { 0 }
-
-        // Keep track of the max height of each row
-        val rowHeights = IntArray(rows) { 0 }
-
-        // Don't constrain child views further, measure them with given constraints
-        // List of measured children
-        val placeables = measurables.mapIndexed { index, measurable ->
-
-            // Measure each child
-            val placeable = measurable.measure(constraints)
-
-            // Track the width and max height of each row
-            val row = index % rows
-            rowWidths[row] += placeable.width
-            rowHeights[row] = max(rowHeights[row], placeable.height)
-
-            placeable
-        }
-        // Grid's width is the widest row
-        val width = rowWidths.maxOrNull()
-            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
-
-        // Grid's height is the sum of the tallest element of each row
-        // coerced to the height constraints
-        val height = rowHeights.sumOf { it }
-            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
-
-        // Y of each row, based on the height accumulation of previous rows
-        val rowY = IntArray(rows) { 0 }
-        for (i in 1 until rows) {
-            rowY[i] = rowY[i-1] + rowHeights[i-1]
-        }
-        // Set the size of the parent layout
-        layout(width, height) {
-            // x cord we have placed up to, per row
-            val rowX = IntArray(rows) { 0 }
-
-            placeables.forEachIndexed { index, placeable ->
-                val row = index % rows
-                placeable.placeRelative(
-                    x = rowX[row],
-                    y = rowY[row]
-                )
-                rowX[row] += placeable.width
             }
         }
     }
 }
 
 @Composable
-fun Chip(modifier: Modifier = Modifier, text: String){
-    Card(
-        modifier = modifier,
-        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
-        shape = RoundedCornerShape(8.dp)
-    ){
-        Row(
-            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Box(
-                modifier = Modifier
-                    .size(16.dp, 16.dp)
-                    .background(color = MaterialTheme.colors.secondary)
-            )
-            Spacer(Modifier.width(4.dp))
-            Text(text = text)
-        }
-    }
-}
-
-//@Preview
-@Composable
-fun ChipPreview(){
-    ComposeLayoutsTheme {
-        LayoutsCodelab()
-    }
-}
-
-val topics = listOf(
-    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
-    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
-    "Religion", "Social sciences", "Technology", "TV", "Writing"
-)
-@Composable
-fun BodyContent(modifier : Modifier = Modifier){
-    Row(modifier = modifier
-        .background(color = Color.LightGray)
-        .padding(16.dp)
-        .size(200.dp)
-        .horizontalScroll(rememberScrollState())) {
-        StaggeredGrid {
-            for (topic in topics) {
-                Chip(modifier = Modifier.padding(8.dp), text = topic)
-            }
-        }
-    }
-}
-
-@Composable
-fun ConstraintLayoutContent(){
-    ConstraintLayout {
-        val (button1, button2, text) = createRefs()
-
-        Button(
-            onClick = { /*TODO*/ },
-            // Assign reference "button" to the Button composable
-            // and constrain it to the top of the ConstraintLayout
-            modifier = Modifier.constrainAs(button1) {
-                top.linkTo(parent.top, margin = 16.dp)
-            }
-        ) {
-            // Assign reference "text" to the Text composable
-            // and constrain it to the bottom of the Button composable
-            Text("Button 1")
-        }
-
-        Text("Text", Modifier.constrainAs(text) {
-            top.linkTo(button1.bottom, margin = 16.dp)
-            centerAround(button1.end)
-        })
-
-        val barrier = createEndBarrier(button1, text)
-
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier.constrainAs(button2) {
-                top.linkTo(parent.top, margin = 16.dp)
-                start.linkTo(barrier)
-            }
-        ) {
-            Text("Button 2")
-        }
-    }
-}
-@Preview
-@Composable
-fun ConstraintLayoutContentPreview() {
-    ComposeLayoutsTheme {
-        ConstraintLayoutContent()
-    }
-}
-
-@Composable
-fun LargeConstraintLayout(){
-    ConstraintLayout {
-        val text = createRef()
-
-        val guideline = createGuidelineFromStart(fraction = 0.5f)
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String){
+    Row(modifier = modifier){
         Text(
-            "This is a very very very very very very very long text",
-            Modifier.constrainAs(text) {
-                linkTo(start = guideline, end = parent.end)
-                width = Dimension.preferredWrapContent.atLeast(100.dp)
-            }
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
         )
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+
     }
-
 }
-
 @Preview
 @Composable
-fun LargeConstraintLayoutPreview() {
+fun TwoTextsPreview() {
     ComposeLayoutsTheme {
-        LargeConstraintLayout()
-    }
-}
-
-@Composable
-fun DecoupledConstraintLayout(){
-    BoxWithConstraints {
-        val constraints = if(maxWidth < maxHeight){
-            decoupledConstraints(margin = 16.dp)
-        } else {
-            decoupledConstraints(margin = 32.dp)
-        }
-        ConstraintLayout(constraints) {
-            Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.layoutId("button")
-            ) {
-                Text("Button")
-            }
-            Text("Text")
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
         }
     }
 }
-
-private fun decoupledConstraints(margin: Dp): ConstraintSet {
-    return ConstraintSet{
-        val button = createRefFor("button")
-        val text = createRefFor("text")
-
-        constrain(button){
-            top.linkTo(parent.top, margin = margin)
-        }
-        constrain(text){
-            top.linkTo(button.bottom, margin = margin)
-        }
-    }
-}
-
