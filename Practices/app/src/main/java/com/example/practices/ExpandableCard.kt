b19de0fcbaa8b8d5cfbc5f27193417270b667bbf
comye1 <yewon2664@gmail.com>
2021-07-07 01:18:07 +0900

    Expandable Card!!

--- Practices/app/src/main/java/com/example/practices/ExpandableCard.kt
+++ Practices/app/src/main/java/com/example/practices/ExpandableCard.kt
@@ -2,6 +2,7 @@ package com.example.practices
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,9 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.practices.ui.theme.Shapes
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 
 @ExperimentalMaterialApi
 @Composable
@@ -27,6 +31,10 @@ fun ExpandableCard(){
     var expandedState by remember {
         mutableStateOf(false)
     }
+    val rotationState by animateFloatAsState(
+        targetValue = if(expandedState) 180f else 0f
+    )
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -52,13 +60,16 @@ fun ExpandableCard(){
                     modifier = Modifier
                         .weight(6f),
                     text = "My Title",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
 //                    fontSize = MaterialTheme.typography.h6,
 //                    fontWeight = FontWeight.Bold
                 )
                 IconButton(
                     modifier = Modifier
                         .alpha(ContentAlpha.medium)
-                        .weight(1f),
+                        .weight(1f)
+                        .rotate(rotationState),
                     onClick = {
                         expandedState = !expandedState
                     }) {
@@ -67,6 +78,19 @@ fun ExpandableCard(){
                         contentDescription = "Drop Down Arrow"
                     )
                 }
+            }
+            if(expandedState) {
+                Text(
+                    text = "It is a long established fact that a reader will be distracted " +
+                            "by the readable content of a page when looking at its layout. " +
+                            "The point of using Lorem Ipsum is that it has a more-or-less " +
+                            "normal distribution of letters, as opposed to using 'Content here, " +
+                            "content here', making it look like readable English. ",
+                    fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                    fontWeight = FontWeight.Normal,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
     }
--- Practices/app/src/main/java/com/example/practices/MainActivity.kt
+++ Practices/app/src/main/java/com/example/practices/MainActivity.kt
@@ -3,8 +3,18 @@ package com.example.practices
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.practices.ui.theme.PracticesTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,10 +23,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticesTheme {
-                Surface(color = MaterialTheme.colors.background) {
-
+                Column (modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+                    .padding(24.dp)
+                ) {
+                  ExpandableCard()
                 }
             }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun DefaultPreview() {
+    PracticesTheme {
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .padding(24.dp)
+        ) {
+            ExpandableCard()
         }
     }
 }
