2b60e1e34dce4f7d0fa43f7765ccd7bf6eeee57a
comye1 <yewon2664@gmail.com>
2021-07-15 00:40:41 +0900

    navigation but error occurs

--- NavigationForBiginners/app/src/main/java/com/example/navigationforbiginners/MainActivity.kt
+++ NavigationForBiginners/app/src/main/java/com/example/navigationforbiginners/MainActivity.kt
@@ -14,25 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationForBiginnersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Navigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NavigationForBiginnersTheme {
-        Greeting("Android")
     }
 }
