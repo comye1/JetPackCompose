f82da481a5c4646ed3d7459b4b36e4c51614268c
comye1 <yewon2664@gmail.com>
2021-07-22 15:29:40 +0900

    Travel App 진행중

--- NavigationForBiginners/app/src/main/java/com/example/navigationforbiginners/Travel/ui/features/SplashScreen.kt
+++ NavigationForBiginners/app/src/main/java/com/example/navigationforbiginners/Travel/ui/features/SplashScreen.kt
@@ -1,4 +1,23 @@
 package com.example.navigationforbiginners.Travel.ui.features
 
-class SplashScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Red))
+}
+
+@Preview
+@Composable
+fun SplashPreview() {
+
 }
--- NavigationForBiginners/app/src/main/java/com/example/navigationforbiginners/TravelApp.kt
+++ NavigationForBiginners/app/src/main/java/com/example/navigationforbiginners/TravelApp.kt
@@ -1,15 +1,49 @@
 package com.example.navigationforbiginners
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationforbiginners.Travel.ui.features.SplashScreen
+import com.example.navigationforbiginners.ui.theme.NavigationForBiginnersTheme
+import com.google.accompanist.insets.ProvideWindowInsets
 
 @Preview
 @Composable
 fun TravelAppPreview() {
-    TravelApp()
+    NavigationForBiginnersTheme {
+
+
+        TravelApp()
+
+    }
 }
 
 @Composable
 fun TravelApp() {
+    val navController = rememberNavController()
+
+    ProvideWindowInsets {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = "splash"
+            ) {
+                composable("splash"){
+                    SplashScreen(navController)
+                }
+            }
+        }
+
+    }
 
 }
