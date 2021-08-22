61e7ead2b0d8ebfb04b0e34cc877b2fe1e0ac511
comye1 <yewon2664@gmail.com>
2021-08-22 23:35:49 +0900

    이름 오타 수정

--- Practices/app/src/main/java/com/example/practices/MainActivity.kt
+++ Practices/app/src/main/java/com/example/practices/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.ExperimentalComposeUiApi
-import com.example.practices.my.AnimationGuageBars
+import com.example.practices.my.AnimationGaugeBars
 
 class MainActivity : ComponentActivity() {
     @ExperimentalComposeUiApi
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AnimationGuageBars()
+            AnimationGaugeBars()
 //            TextFields()
 //            SelectionScreen()
 //            PlantHomeScreen()
